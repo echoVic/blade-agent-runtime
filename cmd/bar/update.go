1493e76c8772e63877d97b0a9c21f82397521bf8
echoVic <echovic@163.com>
2026-02-04 11:45:11 +0800

    chore: release v0.0.20

--- cmd/bar/update.go
+++ cmd/bar/update.go
@@ -16,7 +16,7 @@ const (
 	repoOwner      = "echoVic"
 	repoName       = "blade-agent-runtime"
 	installURL     = "https://echovic.github.io/blade-agent-runtime/install.sh"
-	currentVersion = "0.0.19"
+	currentVersion = "0.0.20"
 )
 
 func updateCmd() *cobra.Command {
